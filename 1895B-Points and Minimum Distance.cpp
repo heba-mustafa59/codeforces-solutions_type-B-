8f1336b8e823519a476ceabd1e4257d1b715ba68
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-01 16:25:51 +0300

    1895B-Points and Minimum Distance

---
+++ 1895B-Points and Minimum Distance.cpp
@@ -0,0 +1,28 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        vector <long long >a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        sort(a.begin(), a.end());
+        cout << max(a[0] * a[1], a[n - 1] * a[n - 2]) << endl;
+    }
+    return 0;
+}
