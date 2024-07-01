282032c921395a332c95639fc073747e066d7af4
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-01 19:34:01 +0300

    1720B - Interesting Sum

---
+++ 1720B - Interesting Sum.cpp
@@ -0,0 +1,29 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        vector <long long >v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+        }
+        sort(v.begin(), v.end());
+        long long ma = v[n - 1] - v[0] + v[n - 2] - v[1];
+        cout << ma << "\n";
+
+    }
+    return 0;
+}
