9443df90ac89e8e4c1939933c33e493c817b2464
𝐇𝐞𝐛𝐚 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-17 17:27:34 +0300

    B - Atilla's Favorite Problem

---
+++ B - Atilla's Favorite Problem.cpp
@@ -0,0 +1,20 @@
+﻿/*
+      yildiz
+*/
+#define ll long long
+#define habboosh main
+#include <bits/stdc++.h>
+using namespace std;
+
+int habboosh() {
+    int t; cin >> t;
+    while (t--) {
+        char ch = 'a';
+        int size; cin >> size;
+        string s; cin >> s;
+        for (int i = 0; i < size; i++)
+            ch = max(ch, s[i]);
+        cout << int(ch - 'a' + 1) <<  "\n";
+    }
+    return 0;
+}
