7921587b99001de3dfe2267ee2aa71eb40f7cb0c
𝐇𝐞𝐛𝐚 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-17 17:42:29 +0300

    B - Square?

---
+++ B - Square.cpp
@@ -0,0 +1,32 @@
+﻿/*
+      yildiz
+*/
+#define ll long long
+#define habboosh main
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int habboosh() {
+
+    int x, a, b, c, d;
+    cin >> x;
+    while (x--) {
+        cin >> a >> b >> c >> d;
+        if (max(a, b) == max(c, d)) {
+            if (min(a, b) + min(c, d) == max(a, b))
+                cout << "Yes\n";
+
+            else cout << "No\n";
+        }
+        else if (min(a, b) == min(c, d)) {
+            if (max(a, b) + max(c, d) == min(a, b))
+                cout << "Yes\n";
+
+            else cout << "No\n";
+        }
+        else cout << "No\n";
+
+    }
+    return 0;
+}
