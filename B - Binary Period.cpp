f13d2bda6f72a42da9353af48a31674b5aeefaa7
𝐇𝐞𝐛𝐚 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-17 17:40:54 +0300

    B - Binary Period

---
+++ B - Binary Period.cpp
@@ -0,0 +1,25 @@
+﻿/*
+      yildiz
+*/
+#define ll long long
+#define habboosh main
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int habboosh() {
+	ll  n;
+		string x;
+		cin >> n;
+		while (n--) {
+			cin >> x;
+			sort(x.begin(), x.end());
+			if (x[0] == x[x.length() - 1])
+				cout << x;
+			else
+				for (int i = 0; i < (x.length()); i++)
+					cout << 1 << 0;
+			cout <<  "\n";
+		}
+		return 0;
+	}
